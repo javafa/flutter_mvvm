96be6095c4f34858562440345c91557c0572c3cb
javafa <nigne2016@gmail.com>
2021-07-12 18:31:41 +0900

    modifid naming

--- lib/view/MainTabView.dart
+++ lib/view/MainTabView.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter_mvvm/view/home/HomeTab.dart';
 import 'package:flutter_mvvm/view/study/StudyTab.dart';
 import 'package:flutter_mvvm/view/study_analysis/StudyAnalysisTab.dart';
@@ -7,21 +9,20 @@ import 'package:get/get.dart';
 
 class MainTabView extends StatelessWidget {
 
-  static const TAB_WIDTH = 150.0;
-  static const TAB_HEIGHT = 50.0;
+  double tabWidth = 140.0;
+  double tabHeight = 80.0;
+
+  List<String> tabList = ["home".tr, "study".tr, "study history".tr, "study analysis".tr];
 
   @override
   Widget build(BuildContext context) {
-
-    var tabList = ["home".tr, "study".tr, "study history".tr, "study analysis".tr];
-
     return DefaultTabController(
       length: tabList.length,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black,
-          toolbarHeight: TAB_HEIGHT,
-          bottom: _tabBar(tabList),
+          toolbarHeight: tabHeight,
+          bottom: _tabBarView(tabList),
         ),
         body: TabBarView(
           children: [
@@ -35,21 +36,21 @@ class MainTabView extends StatelessWidget {
     );
   }
 
-  _tabBar(List<String> labels) {
-    var tablist = List<Tab>.generate(labels.length, (idx) => _tab(labels[idx]));
+  _tabBarView(List<String> labels) {
     return TabBar(
         isScrollable: true,
         indicatorColor: Colors.white,
         labelColor: Colors.white,
         unselectedLabelColor: Colors.white38,
-        tabs: tablist,
+        tabs: List<Tab>.generate(labels.length, (idx) => _tabView(labels[idx])),
     );
   }
 
-  _tab(label) => Tab(child:
-    Container(child: Text(label),
+  _tabView(label) => Tab(
+    child: Container(
+      child: Text(label),
       alignment:Alignment.center,
-      width: TAB_WIDTH,
-      height: TAB_HEIGHT)
+      width: tabWidth,
+      height: tabHeight)
   );
 }
