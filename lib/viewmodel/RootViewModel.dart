bc35851b7ed704a3cbf64e72527ffdc29145aa08
javafa <nigne2016@gmail.com>
2021-07-13 16:16:22 +0900

    etc

--- lib/viewmodel/RootViewModel.dart
+++ lib/viewmodel/RootViewModel.dart
@@ -3,24 +3,8 @@ import 'package:flutter_mvvm/repository/model/response/User.dart';
 import 'package:get/get.dart';
 class RootViewModel extends GetxController
 {
-  var isLoading = true.obs;
-  var userList = List<User>.empty().obs;
-
   @override
   void onInit() {
-    fetchUsers();
     super.onInit();
-  }
-
-  void fetchUsers() async {
-    try {
-      isLoading(true);
-      var products = await UserRepository.getUsers();
-      if (products != null) {
-        userList.assignAll(products);
-      }
-    } finally {
-      isLoading(false);
-    }
   }
 }
